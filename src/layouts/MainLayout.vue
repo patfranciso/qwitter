d00832444cf92c561d3694b25ea740ed2a5f9578
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-15 18:05:38 +0100

    Nav links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,46 +1,56 @@
 <template>
   <q-layout view="lHr lpR fFf">
-
-    <q-header bordered class="bg-white text-black">
+    <q-header class="bg-white text-black">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title class="text-weight-bold">
-          Qwitter
-        </q-toolbar-title>
-
+        <q-toolbar-title class="text-weight-bold"> Qwitter </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer :width="283" show-if-above v-model="leftDrawerOpen" side="left">
+      <q-icon name="fas fa-dove" size="lg" color="primary" class="q-pa-md" />
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-old">Home</q-item-section>
+        </q-item>
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-old">About</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right">
       <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
       },
-
-    }
-  }
-}
+    };
+  },
+};
 </script>
