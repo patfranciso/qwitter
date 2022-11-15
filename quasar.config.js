9059f434b2b96795b85bd2cf1a39108fb9931158
Patrick Omogbeme <patrick.omogbeme@andela.com>
2022-11-15 13:06:02 +0100

    Changed port number

--- quasar.config.js
+++ quasar.config.js
@@ -71,6 +71,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
+      port: 7034,
       open: true, // opens browser window automatically
     },
 
