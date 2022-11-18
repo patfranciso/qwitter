dbf2853e564ff0896eeb5d092f2cac1c251b7874
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-18 18:48:50 +0100

    About page done

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page>
-    <h5>About</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
+        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
+        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
+        Maxime, provident est.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique
+        molestias dolore, ab libero eius? Voluptatum quaerat architecto ex
+        blanditiis, perspiciatis dolor quisquam labore fuga repellendus minima!
+        Maxime, provident est.
+      </p>
+    </div>
   </q-page>
 </template>
 
