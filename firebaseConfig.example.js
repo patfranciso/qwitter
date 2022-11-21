51938bc37bb505d550d7cae08de0db64cb61c2ac
Patrick Omogbeme <patfranciso@gmail.com>
2022-11-21 23:04:42 +0100

    Firebase config

---
+++ firebaseConfig.example.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'xxxxxxx',
+  authDomain: 'xxxxxxxxxxxx',
+  databaseURL: 'https://xxxxxxxxxxxxxxxxx.firebaseio.com',
+  projectId: 'xxxxxxxxxxxxxx',
+  storageBucket: 'xxxxxxxxxxxxxx.appspot.com',
+  messagingSenderId: 'xxxxxxxxxxxxxxxx',
+  appId: 'xxxxxxxxxxxxxxxxxxxxx',
+};
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,7 +1,22 @@
-import { boot } from 'quasar/wrappers'
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../firebaseConfig';
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+  collection,
+  query,
+  where,
+  onSnapshot,
+  orderBy,
+} from 'firebase/firestore';
+import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async (/* { app, router, ... } */) => {
-  // something to do
-})
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+connectFirestoreEmulator(db, 'localhost', 8081);
+const storage = getStorage();
+connectStorageEmulator(storage, 'localhost', 9199);
+
+export { db, collection, query, where, onSnapshot, orderBy };
